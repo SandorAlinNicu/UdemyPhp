ac5385b608b8a819f35bf7c3d69cfc2b222a0a6a
SandorAlinNicu <sandorica1034@gmail.com>
2019-08-13 14:09:28 +0300

    Add section 5

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -1,36 +1,48 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
-	<section class="content">
+<?php include "includes/header.php"; ?>
+    <section class="content">
 
-		<aside class="col-xs-4">
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+    <aside class="col-xs-4">
+        <?php Navigation(); ?>
 
 
-<article class="main-content col-xs-8">
-
-	
-	<?php 
+    </aside><!--SIDEBAR-->
 
 
-/*  Step1: Use a pre-built math function here and echo it
+    <article class="main-content col-xs-8">
 
 
-	Step 2:  Use a pre-built string function here and echo it
+        <?php
 
 
-	Step 3:  Use a pre-built Array function here and echo it
-
- */
+        /*  Step1: Use a pre-built math function here and echo it
 
 
-    ?>
+            Step 2:  Use a pre-built string function here and echo it
 
 
+            Step 3:  Use a pre-built Array function here and echo it
+
+         */
+
+        //Step1:
+
+        echo sqrt(144) . "<br>";
+
+        //Step2:
+
+        $string = "am vrut sa zic";
+
+        echo strpos($string, " ") . "<br>";
+
+        //Step3:
+
+        $list = array(12, 564, 65, "da");
+
+        echo implode(", ", $list);
+
+        ?>
 
 
-
-</article><!--MAIN CONTENT-->
+    </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
