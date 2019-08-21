4b853d5e98b1577d755e5befe4f6effd27294fae
SandorAlinNicu <sandorica1034@gmail.com>
2019-08-21 15:57:05 +0300

    Add section 9

--- _practical-app/9.php
+++ _practical-app/9.php
@@ -25,9 +25,44 @@
 
             Step 3 - Start a session and set it to value, any value you want.
         */
+        ?>
+
+        <!-- Step 1: -->
+        <a href="?x=10&y=20&z=30">Click Here</a>
+
+        <?php echo "<br>";
+        print_r($_GET); ?>
+
+        <?php
+
+        $x = $_GET['x'];
+        $y = $_GET['y'];
+        $z = $_GET['z'];
+
+        echo '<br>' . $x . ' ' . $y . ' ' . $z;
 
         ?>
 
+        <?php
+
+        //Step 2:
+
+        $name = "Edgar";
+        $value = "oOoOoOoOoOoOoOoOoOoOoOoO";
+        $expire = time() + (60 * 60 * 24 * 7 * 4);
+        setcookie($name, $value, $expire);
+
+        ?>
+
+        <?php
+
+        //Step 3:
+
+        session_start();
+
+        $_SESSION['Alin'] = 100;
+
+        ?>
 
     </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
